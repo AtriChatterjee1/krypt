6e105ee444fd8dd07a1aaa6aa1e06031feba2279
Atri Chattopadhyay <112633183+AtriChatterjee1@users.noreply.github.com>
2023-10-13 00:59:40 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks : {
     sepolia : {
       url: 'https://eth-sepolia.g.alchemy.com/v2/dXTrur0D3mcdxd26kUjjhN85FHxaNizr' ,
-      accounts : ['38789e9b13d48fd2e727f75d89158ae43e8695f6f00e2e2601594e03dbc0e40e']
+      accounts : [${{secrets.PRIVATE_KEY}}]
     }
   }
 }
